0e11ff0280658bef53352e2a46b5e3bf31514385
tuffy <102904603+tc-Tuffy@users.noreply.github.com>
2023-04-09 13:50:08 +0800

    Update buffer.cpp

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -3,7 +3,7 @@
 #include "buffer.h"
 
 using namespace std;
-
+// 用迭代器找到对应的key，返回对应val的指针。
 VAL_t * Buffer::get(KEY_t key) const {
     entry_t search_entry;
     set<entry_t>::iterator entry;
@@ -24,13 +24,13 @@ VAL_t * Buffer::get(KEY_t key) const {
 vector<entry_t> * Buffer::range(KEY_t start, KEY_t end) const {
     entry_t search_entry;
     set<entry_t>::iterator subrange_start, subrange_end;
-
+    // 利用迭代器的二分查找函数，获取上界和下界。
     search_entry.key = start;
     subrange_start = entries.lower_bound(search_entry);
 
     search_entry.key = end;
     subrange_end = entries.upper_bound(search_entry);
-
+    // 用迭代器构造vector对象返回
     return new vector<entry_t>(subrange_start, subrange_end);
 }
 
@@ -44,7 +44,7 @@ bool Buffer::put(KEY_t key, VAL_t val) {
     } else {
         entry.key = key;
         entry.val = val;
-
+        // std::tie函数用于解包pair对象，set不能插入相同key的元素，失败返回false和对应相同key的迭代器。
         tie(it, found) = entries.insert(entry);
 
         // Update the entry if it already exists
